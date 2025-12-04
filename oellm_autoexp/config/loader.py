088eb7525f86b0a5154a4155984337c9b3d0adc1
Risto Luukkonen <risto.m.luukkonen@gmail.com>
2025-12-04 16:34:17 +0200

    Refactor: remove rocm backend

--- oellm_autoexp/config/loader.py
+++ oellm_autoexp/config/loader.py
@@ -49,7 +49,6 @@ def _ensure_registrations() -> None:
     for module in (
         "oellm_autoexp.backends.base",
         "oellm_autoexp.backends.megatron_backend",
-        "oellm_autoexp.backends.megatron_rocm_backend",
         "oellm_autoexp.monitor.actions",
         "oellm_autoexp.monitor.states",
         "oellm_autoexp.monitor.watcher",
