e39cc28b4be98b63fdb08a1f484f9f223b7484c0
Risto Luukkonen <risto.m.luukkonen@gmail.com>
2025-12-18 13:43:58 +0200

    testing

--- run_0.7B_115M_ablations.sh
+++ run_0.7B_115M_ablations.sh
@@ -4,6 +4,6 @@ source set_env.sh
 export PYTHONPATH=$PYTHONPATH:$pwd
 
 
-OELLM_MEGATRON_SCHEMA_ONLY=1 python3 scripts/run_autoexp_container.py --config-ref experiments/rluukkon/moe_0.7B_115M_dense_input_output.yaml --no-monitor
-OELLM_MEGATRON_SCHEMA_ONLY=1 python3 scripts/run_autoexp_container.py --config-ref experiments/rluukkon/moe_0.7B_115M_shared_expert.yaml --no-monitor
-OELLM_MEGATRON_SCHEMA_ONLY=1 python3 scripts/run_autoexp_container.py --config-ref experiments/rluukkon/moe_0.7B_115M.yaml --no-monitor
+# python3 scripts/run_autoexp_container.py --config-ref experiments/rluukkon/moe_0.7B_115M_dense_input_output.yaml --no-monitor
+# python3 scripts/run_autoexp_container.py --config-ref experiments/rluukkon/moe_0.7B_115M_shared_expert.yaml --no-monitor
+python3 scripts/run_autoexp_container.py --config-ref experiments/rluukkon/moe_0.7B_115M.yaml --no-monitor
