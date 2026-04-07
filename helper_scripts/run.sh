18f5ad71f7a59fb9a95271c860199cd7b586a24e
Ville Komulainen <vmjkom@utu.fi>
2026-04-07 11:07:04 +0200

    refactor: clean up scripts

--- helper_scripts/run.sh
+++ helper_scripts/run.sh
@@ -5,4 +5,4 @@ module load GCCcore/14.3.0
 module load Python/3.13.5
 source /e/project1/e-sta-openeurollm/komulainen1_jupiter/oellm-autoexp/.venv/bin/activate
 export WANDB_ENTITY=openeurollm-project
-python3 scripts/run_autoexp.py --submit-and-exit --config-name experiments/ville/jupiter/jupiter_qwen-30B.yaml --debug $@
+python3 scripts/run_autoexp.py --submit-and-exit --config-name experiments/ville/jupiter/jupiter_qwen-30B.yaml $@
