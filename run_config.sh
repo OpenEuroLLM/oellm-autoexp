6e53318c81812406fbd09060e4413374bdbf5cf4
dianaonutu <d.onutu@tue.nl>
2026-04-29 14:02:55 +0200

    add hydra configs to launcher

--- run_config.sh
+++ run_config.sh
@@ -4,12 +4,15 @@ config=$1 # experiments/jllop/...
 shift
 
 extra_args=""
+hydra_overrides=""
 for arg in "$@"; do
   if [ "$arg" = "--no-submit" ]; then
     extra_args="$extra_args --no-submit"
+  else
+    hydra_overrides="$hydra_overrides $arg"
   fi
 done
 
 source use_env.sh
 
-PYTHONPATH=. python scripts/run_autoexp.py --config-name $config $extra_args
+PYTHONPATH=. python scripts/run_autoexp.py --config-name $config $extra_args $hydra_overrides
